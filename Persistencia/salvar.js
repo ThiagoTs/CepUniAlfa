7207d8997e833cc17f1cb571055ebccd1c3ccd49
Thiago dos Santos <thiagots_outlook.com>
2019-10-19 14:42:03 -0300

    Novo CSS

--- Persistencia/salvar.js
+++ Persistencia/salvar.js
@@ -4,40 +4,37 @@ function converter() {
     var obj = new Object();
     obj.Q1 = Radio1();
 
-    obj.Q2= Radio2();
+    obj.Q2 = Radio2();
 
     obj.Q3 = Radio3();
 
     objJson = JSON.stringify(obj);
+    console.log(objJson);
 
+    if(Radio1() == null || Radio2() == null || Radio3()== null){
+        alert("Escolha pelo menos uma opção em todas as perguntas");
+        voltar();
+    }else{
 
-    console.log(objJson)
+        var http = new XMLHttpRequest();
+        var url = 'http://137.135.71.213:5000/api/answer';
+        var params = objJson;
+        http.open('POST', url, true);
     
-
-
-    var http = new XMLHttpRequest();
-    var url = 'http://137.135.71.213:5000/api/answer';
-    var params = objJson;
-    http.open('POST', url, true);
-
-    //Send the proper header information along with the request
-    http.setRequestHeader('Content-type', 'application/json');
-
-    http.onreadystatechange = function () {//Call a function when the state changes.
-        if (http.readyState == 4 && http.status == 200) {
-            alert(http.responseText);
+        //Send the proper header information along with the request
+        http.setRequestHeader('Content-type', 'application/json');
+    
+        http.onreadystatechange = function () {//Call a function when the state changes.
+            if (http.readyState == 4 && http.status == 200) {
+                alert(http.responseText);
+            }
         }
+        http.send(params);
+        alert("Salvo, Obrigado!");
+        voltar();
     }
-    http.send(params);
-    alert('Obrigado!');
-    voltar();
-
-    
-
-
 
 }
-
 
 function Radio1() {
     var form = document.getElementById('questao1');
@@ -51,7 +48,7 @@ function Radio1() {
         }
 
     }
-    alert('Selecione alguma opção na questão 1');
+    
 
 }
 function Radio2() {
@@ -61,13 +58,13 @@ function Radio2() {
     Radio = form.questao2;
     for (var i = 0; i < Radio.length; i++) {
         if (Radio[i].checked) {
-            // msg = "Selecionado: " + Radio[i].value;
+            //  msg = "Selecionado: " + Radio[i].value);
 
             return Radio[i].value;
         }
 
     }
-    alert('Selecione alguma opção na questão 2');
+    
 
 }
 function Radio3() {
@@ -83,13 +80,9 @@ function Radio3() {
 
         }
     }
-    alert('Selecione alguma opção na questão 3');
+   
 
 }
-
-
-
-
 
 
 
